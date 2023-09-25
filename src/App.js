c94b2fc435d7aef1bb0139ab9d1059405582ac00
agdgb <amanuelgirma@hotma>
2023-09-25 14:26:30 +0300

    adding open graph protocols

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import logo from './Logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      Home Page
-    </div>
+    <>
+      <Header props={logo}/>
+      <main>main</main>
+      <footer>footer</footer>
+    </>
   );
 }
 
